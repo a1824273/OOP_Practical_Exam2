41634b4ddc07e880a51391a3b642be97b5991128
Tyson Tang <a1824273@student.adelaide.edu.au>
2022-03-31 11:30:47 +1030

    commit4

---
+++ function-4-1.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+#include "function-4-1.h"
+
+BusCounter::BusCounter(int cap)
+{
+    int capacity = cap;
+    bus_count = 0;
+}
+
+void BusCounter::board()
+{
+    if (bus_count < capacity)
+    {
+        bus_count++;
+    }
+}
+
+void BusCounter::exit()
+{
+    if(bus_count > 0)
+    {
+        bus_count--;
+    }
+}
+
+BusCounter::get_count()
+{
+    return (bus_count);
+}
